7fd0d69987d8c4e8ceb1948d53216025888fe1c5
ImeShoukat <imedia.shoukat@mhs.unsoed.ac.id>
2025-09-23 19:54:03 +0700

    Tugas 2 Pertemuan 4

--- README.md
+++ README.md
@@ -7,4 +7,11 @@
 - **Shift KRS:** C
 - **Shift Akhir:** D
 
+### pertemuan 2:
 ![Screenshot Apk](screenshoot/Hasil-SS.jpeg)
+
+### pertemuan 3:
+![Screenshot Apk](screenshoot/move_page.gif)
+
+### pertemuan 4:
+![Screenshot Apk](screenshoot/list-buku.gif)
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,16 +33,24 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    viewBinding{
+        enable = true
+    }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.activity.ktx)
 }
